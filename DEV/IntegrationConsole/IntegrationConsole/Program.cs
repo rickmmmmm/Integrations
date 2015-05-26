756645658999c7c5c108d16b00e9fb06ef624e78
jjohnston@hayessoft.com <jjohnston@hayessoft.com>
2015-05-26 19:16:27 +0000

    git-tfs-id: [https://hayessoftware.visualstudio.com/]$/Hayes Integration;C5567

--- DEV/IntegrationConsole/IntegrationConsole/Program.cs
+++ DEV/IntegrationConsole/IntegrationConsole/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IntegrationConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            using (TIPWebIT.WebServiceSoapClient client = new TIPWebIT.WebServiceSoapClient())
+            {
+                client.ExternalBulkUpdate("SAP", "SAP1234!");
+                return 0;
+            }
         }
     }
 }
