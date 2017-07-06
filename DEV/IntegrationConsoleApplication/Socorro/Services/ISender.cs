f003022d398043bf595efb13cbeb0da1b964408b
dcathcart@hayessoft.com <dcathcart@hayessoft.com>
2017-07-06 20:51:34 +0000

    Branched from $/Hayes Integration/DEV/IntegrationConsoleApplication/MesaPS
    git-tfs-id: [https://hayessoftware.visualstudio.com/]$/Hayes Integration;C11581

---
+++ DEV/IntegrationConsoleApplication/Socorro/Services/ISender.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface ISender
+    {
+        void send(IMessage message);
+        void sendAsync(IMessage message);
+    }
+}
