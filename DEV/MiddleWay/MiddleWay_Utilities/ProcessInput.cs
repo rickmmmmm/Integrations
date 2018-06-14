5a8674679147e9ed9fc1d6073918b46e7c8f3f4a
gcollazo@hayessoft.com <gcollazo@hayessoft.com>
2018-06-14 21:42:14 +0000

    git-tfs-id: [https://hayessoftware.visualstudio.com/]$/Hayes Integration;C14845

---
+++ DEV/MiddleWay/MiddleWay_Utilities/ProcessInput.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiddleWay_Utilities
+{
+    public class ProcessInput
+    {
+        #region Variables and Properties
+
+        private List<string> _parameters;
+
+        #endregion Variables and Properties
+
+        #region Constructor
+
+        public ProcessInput(List<string> parameters)
+        {
+            _parameters = parameters;
+        }
+
+        #endregion Constructor
+
+        #region Functions
+
+        /// <summary>
+        /// Get all options (single dash) in the parameters
+        /// </summary>
+        /// <returns></returns>
+        public List<string> ReadOptions()
+        {
+            var data = (from options in _parameters
+                        where options.StartsWith("-") &&
+                              !options.StartsWith("--")
+                        select options).ToList();
+
+            return data;
+        }
+
+        /// <summary>
+        /// Check if the provider parameter is contained in the input
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool HasParameter(string parameter)
+        {
+            if (!parameter.StartsWith("--"))
+            {
+                parameter = "--" + parameter;
+            }
+
+            var data = (from options in _parameters
+                        where options == parameter
+                        select options).Count();
+
+            return data == 1;
+        }
+
+        /// <summary>
+        /// Return the first values associated with the specified parameter.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public string ReadParameterValue(string parameter)
+        {
+            if (!parameter.StartsWith("--"))
+            {
+                parameter = "--" + parameter;
+            }
+
+            var indexOfParam = _parameters.IndexOf(parameter);
+            var value = string.Empty;
+            if (indexOfParam >= 0)
+            {
+                var startIndex = indexOfParam + 1;
+
+                if (!_parameters[startIndex].StartsWith("-") && !_parameters[startIndex].StartsWith("--"))
+                {
+                    value = _parameters[startIndex];
+                }
+            }
+            return value;
+
+        }
+
+        /// <summary>
+        /// Return all values associated with the specified parameter. All values
+        /// before the next option or parameter
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public List<string> ReadParameterValues(string parameter)
+        {
+            if (!parameter.StartsWith("--"))
+            {
+                parameter = "--" + parameter;
+            }
+
+            var indexOfParam = _parameters.IndexOf(parameter);
+            var values = new List<string>();
+            if (indexOfParam >= 0)
+            {
+                for (var startIndex = indexOfParam + 1; startIndex < _parameters.Count(); startIndex++)
+                {
+                    if (_parameters[startIndex].StartsWith("-") || _parameters[startIndex].StartsWith("--"))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        values.Add(_parameters[startIndex]);
+                    }
+                }
+            }
+            return values;
+
+        }
+
+        #endregion Functions
+    }
+}
---
+++ DEV/MiddleWay/MiddleWay_Utilities/Utilities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiddleWay_Utilities
+{
+    public static class Utilities
+    {
+        public static string ParseException(Exception ex)
+        {
+            var result = new StringBuilder();
+            //var message = "";
+            //    var stack = "";
+            result.Append(ex.Message);
+            result.AppendLine(ex.StackTrace);
+            if (ex.InnerException != null)
+            {
+                Exception innerEx;
+                innerEx = ex.InnerException;
+                while (innerEx != null)
+                {
+                    result.Append(innerEx.Message);
+                    result.AppendLine(innerEx.StackTrace);
+                    innerEx = innerEx.InnerException;
+                }
+            }
+
+            return result.ToString();
+
+        }
+
+    }
+}
