08c8cb26ee0e5bb1ad00bcf36314af515553a1ba
rgailey@hayessoft.com <rgailey@hayessoft.com>
2018-07-06 13:47:56 +0000

    Logging created and tested
    git-tfs-id: [https://hayessoftware.visualstudio.com/]$/Hayes Integration;C15083

--- DEV/MiddleWay/MiddleWay_Utilities/Logging.cs
+++ DEV/MiddleWay/MiddleWay_Utilities/Logging.cs
@@ -1,50 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//using Model;
-//using DataAccess;
+using System.IO;
 
 namespace MiddleWay_Utilities
 {
     public class Logging
     {
-        //private IRepository _repo;
-
-        //public Logging(IRepository repo)
-        //{
-        //    _repo = repo;
-        //}
-
-        public enum ChangeType { Error, Activity, RejectRecord }
-        public void log(string message, string actionName, ChangeType type)
+        public static bool WriteToLog(string ClientCode, string MessageType, string Message, int ProcessTaskID, string ProcessTaskName)
         {
+            // MessageType Should be (Informational, Warning, Successful, Failure)
 
-            if (type == ChangeType.Error)
+            // Set to defaults if not provided
+            if (String.IsNullOrEmpty(ClientCode))
+                ClientCode = "MiddleWay";
+
+            if (String.IsNullOrEmpty(MessageType))
+                MessageType = "Informational";
+
+            if (String.IsNullOrEmpty(ProcessTaskID.ToString()))
+                ProcessTaskID = 0;
+
+            if (String.IsNullOrEmpty(ProcessTaskName))
+                ProcessTaskName = "Default Process Name";
+
+            if (String.IsNullOrEmpty(Message))
+                Message = "Default Message - No Message Provided";
+
+            //string LogFilePath = "C:\\Temp\\Logs";  // For Testing only
+            string LogFilePath = Directory.GetCurrentDirectory() + "\\Logs\\";
+
+            if (String.IsNullOrEmpty(LogFilePath))
+                LogFilePath = "C:\\Temp\\Logs\\";
+
+            if (LogFilePath.Substring(LogFilePath.Length - 1, 1) != "\\")
             {
-                //_repo.logError(message, actionName);
+                LogFilePath = LogFilePath + "\\";
             }
 
-            //_repo.logAction(actionName, message);
+            string LogFileDate = System.DateTime.Now.Month.ToString("00") + System.DateTime.Now.Day.ToString("00") + System.DateTime.Now.Year.ToString("0000");
+            string LogFileName = ClientCode + "_" + LogFileDate + ".log";
+
+            string LogEntry = "";
+
+            // Cleanup message here to remove crt returns, line feeds, and tabs
+
+            if (!File.Exists(LogFilePath))
+            {
+                Directory.CreateDirectory(LogFilePath);
+            }
+
+
+            if (File.Exists(LogFilePath + LogFileName))
+            {
+                LogEntry = System.DateTime.Now.ToString() + "|" + ProcessTaskID.ToString() + "|" + ProcessTaskName + "|" + MessageType + "|" + Message;
+            }
+            else
+            {
+                LogEntry = System.DateTime.Now.ToString() + "|0|Starting|Informational|File Creation" + System.Environment.NewLine;
+                LogEntry = LogEntry + System.DateTime.Now.ToString() + "|" + ProcessTaskID.ToString() + "|" + ProcessTaskName + "|" + MessageType + "|" + Message;
+            }
+
+            try
+            {
+                FileStream objFilestream = new FileStream(string.Format("{0}\\{1}", LogFilePath, LogFileName), FileMode.Append, FileAccess.Write);
+
+                StreamWriter objStreamWriter = new StreamWriter((Stream)objFilestream);
+                objStreamWriter.WriteLine(LogEntry);
+
+                objStreamWriter.Close();
+                objFilestream.Close();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
-
-    }
-
-    public class ErrorEventArgs : EventArgs
-    {
-        public string message { get; set; }
-        public string actionName { get; set; }
-        public Logging.ChangeType type { get; set; }
-        public ErrorData Data { get; set; }
-    }
-
-    public class ErrorData
-    {
-        public string Reference { get; set; }
-        public string Reason { get; set; }
-        public string ExceptionMessage { get; set; }
-        public string RejectedValue { get; set; }
-        public int LineNumber { get; set; }
     }
 }
