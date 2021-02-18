d2a775fdea2d925cab62fc3b75cad0b2fed76bf9
Kunal Ashar <kashar@frontlineed.com>
2021-02-18 17:46:46 +0000

    Removed reference to RJ
    git-tfs-id: [https://hayessoftware.visualstudio.com/]$/Hayes Integration;C23535

--- MAIN/ChicagoPS/Archive/PurchaseOrder/intg_data_process.sh
+++ MAIN/ChicagoPS/Archive/PurchaseOrder/intg_data_process.sh
@@ -72,7 +72,7 @@ if [ $fileCount -gt 0 ]; then
     if [ $ENVIRONMENT = "Production" ]; then
         RECIPIENTS="ToAddresses=""support@hayessoft.com""";
     else        
-        RECIPIENTS="ToAddresses=""rgailey@hayessoft.com""";
+        RECIPIENTS="ToAddresses=""Integration_results@hayessoft.com""";
     fi
 TEXTCONTENT="\nThe Chicago Hayes Oracle $TYPE Integration has begun processing files: $processedFiles\n\nIf you have any questions please contact support at 1-800-495-5993 or support@hayessoft.com\n\nHayes Software Systems";
 HTMLCONTENT="<br />The Chicago Hayes Oracle $TYPE Integration has begun processing files: $processedFilesHtml<br /><br />If you have any questions please contact support at 1-800-495-5993 or support@hayessoft.com<br /><br />Hayes Software Systems";
